b0a8b69a53f73229d9742177fd46dfbbb2c19669
37493 <37493@LAPTOP-DTHO96B3>
2022-05-03 12:45:46 +0400

    Add project files.

---
+++ AppTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit;
+using NUnit.Framework;
+
+namespace bookapi.Test
+{
+    [TestFixture]
+    public class AppTest
+    {
+        //private PrimeService _primeService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            //_primeService = new PrimeService();
+        }
+
+        [Test]
+        public void ShouldAnswerWithTrue()
+        {
+            //assertTrue(true);
+           // Assert.IsFalse(result, "1 should not be prime");
+
+        }
+
+    }
+}
